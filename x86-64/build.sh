91f8042a53475e4300d95491387980f8a9c89014
ziyao20 <714420258@qq.com>
2024-12-31 21:32:27 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -22,6 +22,8 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-app-smartdns-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-mosdns-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
